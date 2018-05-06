1d9dd5a5bc55e4e05dfdf68ddcf9c48da9368146
Oleg Budin <aevathari@gmail.com>
2018-05-06 20:56:23 +0300

    Доработан тест на PersistenceManager.
    Сюиту TestNG оставлю, хотя не смог ее внедрить по- человечески, пришлось использовать JUnit

--- app/src/test/java/com/zavijavasoft/jaacad/PersistenceManagerTest.java
+++ app/src/test/java/com/zavijavasoft/jaacad/PersistenceManagerTest.java
@@ -1,23 +1,25 @@
 package com.zavijavasoft.jaacad;
 
-import org.testng.annotations.Test;
+
+import org.junit.Test;
 
 import java.io.File;
-import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.Date;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertNull;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+
 
 public class PersistenceManagerTest {
 
-    @Test
-    void saveLoadTest() {
 
-        File fl = new File("");
-        String s = fl.getAbsolutePath();
+    @Test
+    public void saveLoadTest() {
+
+        File fl = new File("src/test/res");
+
         PersistenceManager manager = new PersistenceManager();
 
         ZonedDateTime zdt = ZonedDateTime.now().minusDays(2);
@@ -31,22 +33,22 @@ public class PersistenceManagerTest {
         GalleryEntity ge2 = new GalleryEntity();
         ge2.setResourceId("2");
         ge2.setState(GalleryEntity.State.IMAGE);
-        ge2.setMarkedAsDead(true);
-        ge2.setLoadedDateTime(Date.from(zdt.toInstant()));
+
 
         GalleryEntity ge3 = new GalleryEntity();
-        ge3.setResourceId("2");
+        ge3.setResourceId("3");
         ge3.setState(GalleryEntity.State.IMAGE);
-        ge3.setLoadedDateTime(Date.from(zdt.toInstant()));
 
         manager.getEntities().add(ge1);
         manager.getEntities().add(ge2);
         manager.getEntities().add(ge3);
 
+        assertNotNull(manager.findEntityById("2"));
+        ge2.setMarkedAsDead(true);
         assertNull(manager.findEntityById("2"));
 
-        manager.saveCachedGallery(true, new File(""));
-        manager.loadCachedGallery(true, new File(""));
+        manager.saveCachedGallery(true, fl.getAbsoluteFile());
+        manager.loadCachedGallery(true, fl.getAbsoluteFile());
 
         assertEquals(manager.getEntities().size(), 2);
         assertNull(manager.findEntityById("2"));
@@ -54,9 +56,11 @@ public class PersistenceManagerTest {
         assertNotNull(ge1r);
         assertEquals(ge1r.getState(), GalleryEntity.State.THUMBNAIL);
 
-        manager.saveCachedGallery(true, new File(""));
-        manager.loadCachedGallery(false, new File(""));
+        manager.saveCachedGallery(true, fl.getAbsoluteFile());
+        manager.loadCachedGallery(false, fl.getAbsoluteFile());
 
         assertEquals(manager.getEntities().size(), 0);
+        File flJson = new File(fl.getAbsoluteFile(), PersistenceManager.JAACAD_GALLERY_JSON);
+        flJson.delete();
     }
 }
