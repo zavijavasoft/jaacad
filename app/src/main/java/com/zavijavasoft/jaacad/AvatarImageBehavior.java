a1a1f30c9e2ca31f4890db147bc19b1a62e2d520
Oleg Budin <aevathari@gmail.com>
2018-05-06 20:57:10 +0300

    Отдокументирован AvatarImageBehavior

--- app/src/main/java/com/zavijavasoft/jaacad/AvatarImageBehavior.java
+++ app/src/main/java/com/zavijavasoft/jaacad/AvatarImageBehavior.java
@@ -12,53 +12,36 @@ import android.view.View;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-@SuppressWarnings("unused")
+
+/**
+ * Класс, реализующий поведение (Behavior) иконки-аватара в CoordinatorLayout
+ */
 public class AvatarImageBehavior extends CoordinatorLayout.Behavior<CircleImageView> {
 
-    private final static float MIN_AVATAR_PERCENTAGE_SIZE = 0.3f;
-    private final static int EXTRA_FINAL_AVATAR_PADDING = 80;
 
-    private final static String TAG = "behavior";
-    private Context mContext;
-    private float globalScale;
+    private final static String TAG = "AvatarImageBehavior";
+    private Context context;
+    private float customFinalHeight;
 
-    private float mCustomFinalYPosition;
-    private float mCustomStartXPosition;
-    private float mCustomStartToolbarPosition;
-    private float mCustomStartHeight;
-    private float mCustomFinalHeight;
-
-    private float mAvatarMaxSize;
-    private float mFinalLeftAvatarPadding;
-    private float mStartPosition;
-    private int mStartXPosition;
-    private float mStartToolbarPosition;
-    private int mStartYPosition;
-    private int mFinalYPosition;
-    private int mStartHeight;
-    private int mFinalXPosition;
-    private float mChangeBehaviorPoint;
+    private int startXPosition;
+    private float startToolbarPosition;
+    private int startYPosition;
+    private int finalYPosition;
+    private int startHeight;
+    private int finalXPosition;
+    private float changeBehaviorPoint;
 
 
     public AvatarImageBehavior(Context context, AttributeSet attrs) {
-        mContext = context;
-        globalScale = context.getResources().getDisplayMetrics().density;
+        this.context = context;
 
         if (attrs != null) {
             TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AvatarImageBehavior);
-            mCustomFinalYPosition = a.getDimension(R.styleable.AvatarImageBehavior_finalYPosition, 0);
-            mCustomStartXPosition = a.getDimension(R.styleable.AvatarImageBehavior_startXPosition, 0);
-            mCustomStartToolbarPosition = a.getDimension(R.styleable.AvatarImageBehavior_startToolbarPosition, 0);
-            mCustomStartHeight = a.getDimension(R.styleable.AvatarImageBehavior_startHeight, 0);
-            mCustomFinalHeight = a.getDimension(R.styleable.AvatarImageBehavior_finalHeight, 0);
-
+            customFinalHeight = a.getDimension(R.styleable.AvatarImageBehavior_finalHeight, 0);
             a.recycle();
         }
 
         init();
-
-        mFinalLeftAvatarPadding = context.getResources().getDimension(
-                R.dimen.spacing_normal);
     }
 
     private void init() {
@@ -66,7 +49,6 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<CircleImageV
     }
 
     private void bindDimensions() {
-        mAvatarMaxSize = mContext.getResources().getDimension(R.dimen.image_width);
     }
 
 
@@ -76,61 +58,76 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<CircleImageV
     }
 
 
+    /**
+     * Основной метод обратного вызова, определяющий повдение
+     *
+     * @param parent     - родительский CoordinatorLayout
+     * @param child      - объект иконки-аватара
+     * @param dependency - объект, от поведения которого зависит поведение иконки - в нашем случае это Toolbar
+     * @return
+     */
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, CircleImageView child, View dependency) {
 
-        float xCurrent = child.getX();
-        float yCurrent = child.getY();
-
-
         maybeInitProperties(child, dependency);
 
-        final int maxScrollDistance = (int) (mStartToolbarPosition);
+        final int maxScrollDistance = (int) (startToolbarPosition);
         float expandedPercentageFactor = dependency.getY() / maxScrollDistance;
 
-        if (expandedPercentageFactor < mChangeBehaviorPoint) {
-            float heightFactor = (mChangeBehaviorPoint - expandedPercentageFactor) / mChangeBehaviorPoint;
+        if (expandedPercentageFactor < changeBehaviorPoint) {
+            //  Тулбар поднимается выше changeBehaviorPoint - движение по горизнотали и масштабирование
+            float heightFactor = (changeBehaviorPoint - expandedPercentageFactor) / changeBehaviorPoint;
 
-            float distanceXToSubtract = ((mStartXPosition - mFinalXPosition)
+            float distanceXToSubtract = ((startXPosition - finalXPosition)
                     * heightFactor) + (child.getHeight() / 2);
-            float distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
+            float distanceYToSubtract = ((startYPosition - finalYPosition)
                     * (1f - expandedPercentageFactor)) + (child.getHeight() / 2);
 
-            float newY = mStartYPosition - distanceYToSubtract;
-            if (newY < mFinalYPosition / 2)
-                newY = mFinalYPosition / 2;
+            float newY = startYPosition - distanceYToSubtract;
+            if (newY < finalYPosition / 2)
+                newY = finalYPosition / 2;
 
-            child.setX(mStartXPosition - distanceXToSubtract);
+            child.setX(startXPosition - distanceXToSubtract);
             child.setY(newY);
 
-            float heightToSubtract = ((mStartHeight - mCustomFinalHeight) * heightFactor);
+            float heightToSubtract = ((startHeight - customFinalHeight) * heightFactor);
 
             CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
-            lp.width = (int) (mStartHeight - heightToSubtract);
-            lp.height = (int) (mStartHeight - heightToSubtract);
+            lp.width = (int) (startHeight - heightToSubtract);
+            lp.height = (int) (startHeight - heightToSubtract);
             child.setLayoutParams(lp);
         } else {
-            float distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
-                    * (1f - expandedPercentageFactor)) + (mStartHeight / 2);
+            //  Тулбар опускается ниже changeBehaviorPoint. Тут движение только по вертикали
+            float distanceYToSubtract = ((startYPosition - finalYPosition)
+                    * (1f - expandedPercentageFactor)) + (startHeight / 2);
 
-            float newX = mStartXPosition - child.getWidth() / 2;
+            float newX = startXPosition - child.getWidth() / 2;
             child.setX(newX);
-            child.setY(mStartYPosition - distanceYToSubtract);
+            child.setY(startYPosition - distanceYToSubtract);
 
             CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
-            lp.width = (int) (mStartHeight);
-            lp.height = (int) (mStartHeight);
+            lp.width = startHeight;
+            lp.height = startHeight;
             child.setLayoutParams(lp);
         }
         return true;
     }
 
+    /**
+     * Сохранение некоторых настроек. В оригинальном примере этого поведения, экран был заморожен в
+     * портретном положении, поэтому проблем не возникало. При смене ориентации, если тулбар
+     * был в верхнем положении (это определялось положением Collapsing toolbar, которое запоминается
+     * системой автоматически), настройки сбивались.
+     * @param parent
+     * @param child
+     * @return
+     */
     @Override
     public Parcelable onSaveInstanceState(CoordinatorLayout parent, CircleImageView child) {
 
         Bundle behaviorState = new Bundle();
-        behaviorState.putInt("startYPosition", mStartYPosition);
-        behaviorState.putFloat("startToolbarPosition", mStartToolbarPosition);
+        behaviorState.putInt("startYPosition", startYPosition);
+        behaviorState.putFloat("startToolbarPosition", startToolbarPosition);
         behaviorState.putParcelable("super", super.onSaveInstanceState(parent, child));
 
         return behaviorState;
@@ -141,36 +138,41 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<CircleImageV
         Bundle behaviorState = (Bundle) state;
         behaviorState.setClassLoader(child.getContext().getClassLoader());
 
-        mStartYPosition = behaviorState.getInt("startYPosition");
-        mStartToolbarPosition = behaviorState.getFloat("startToolbarPosition");
+        startYPosition = behaviorState.getInt("startYPosition");
+        startToolbarPosition = behaviorState.getFloat("startToolbarPosition");
         Parcelable superState = behaviorState.getParcelable("super");
 
         super.onRestoreInstanceState(parent, child, superState);
 
     }
 
+    /**
+     * Метод инициализации переменных, если они не инициализированы
+     * @param child - иконка-аватар
+     * @param dependency - тулбар
+     */
     private void maybeInitProperties(CircleImageView child, View dependency) {
 
-        if (mStartYPosition == 0)
-            mStartYPosition = (int) (dependency.getY() - child.getHeight() / 3);
+        if (startYPosition == 0)
+            startYPosition = (int) (dependency.getY() - child.getHeight() / 3);
 
-        if (mFinalYPosition == 0)
-            mFinalYPosition = (dependency.getHeight() / 2);
+        if (finalYPosition == 0)
+            finalYPosition = (dependency.getHeight() / 2);
 
-        if (mStartHeight == 0)
-            mStartHeight = child.getHeight();
+        if (startHeight == 0)
+            startHeight = child.getHeight();
 
-        if (mStartXPosition == 0)
-            mStartXPosition = (int) (child.getX() + (child.getWidth() / 2));
+        if (startXPosition == 0)
+            startXPosition = (int) (child.getX() + (child.getWidth() / 2));
 
-        if (mFinalXPosition == 0)
-            mFinalXPosition = mContext.getResources().getDimensionPixelOffset(R.dimen.abc_action_bar_content_inset_material) + ((int) mCustomFinalHeight / 2);
+        if (finalXPosition == 0)
+            finalXPosition = context.getResources().getDimensionPixelOffset(R.dimen.abc_action_bar_content_inset_material) + ((int) customFinalHeight / 2);
 
-        if (mStartToolbarPosition == 0)
-            mStartToolbarPosition = dependency.getY();
+        if (startToolbarPosition == 0)
+            startToolbarPosition = dependency.getY();
 
-        if (mChangeBehaviorPoint == 0) {
-            mChangeBehaviorPoint = (child.getHeight() - mCustomFinalHeight) / (2f * (mStartYPosition - mFinalYPosition));
+        if (changeBehaviorPoint == 0) {
+            changeBehaviorPoint = (child.getHeight() - customFinalHeight) / (2f * (startYPosition - finalYPosition));
         }
     }
 
